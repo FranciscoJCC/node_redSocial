0d6c1150c78cc0f48cf5f6b1eeb328825f6dd526
Francisco Cervantes <fjccandelario@gmail.com>
2024-03-09 02:26:36 -0600

    Se agrega la documentación de swagger

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const swaggerUi = require('swagger-ui-express');
 const config = require('../config');
 const user = require('./components/user/network');
 
@@ -8,7 +8,12 @@ const app = express();
 //middleware para recibir datos
 app.use(express.json());
 
+const swaggerDoc = require('./swagger.json');
+
+//Rutas
 app.use('/api/user', user);
+//Documentación de API Swager
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto: ', config.api.port);
